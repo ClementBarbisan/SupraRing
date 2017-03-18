5e7ae706b5970e3062cb3cbbfc01403a919690eb
Guillaume Dilasser <guillaume.dilasser@gmail.com>
2017-03-18 15:42:16 +0100

    Added charging dock

---
+++ Assets/Scripts/ChargingDockBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargingDockBehavior : MonoBehaviour {
+
+    // 
+    Player currentPlayer ;
+    bool playerInside ;
+
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKey(KeyCode.E) && playerInside )
+        {
+            Debug.Log("Charged !");
+            currentPlayer.isCharged = true ;
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Entered the charging dock");
+        if ( other.GetComponent<Player>() )
+        {
+            currentPlayer = other.GetComponent<Player>() ;
+            playerInside = true ;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<Player>())
+        {
+            playerInside = false;
+        }
+    }
+}
