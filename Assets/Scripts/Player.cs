7b8e42ccdeadfccb125186e5d56e0799da5d5472
Guillaume Dilasser <guillaume.dilasser@gmail.com>
2017-03-18 12:47:04 +0100

    Added temperature calculation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,8 +3,15 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 	public Rigidbody2D rb;
-	public float temperature = -273.0f;
+
+	// Thermal properties
+	public float temperature = 77.0f ;
+	public float ambientTemperature = 300.0f ;
+	private float alpha       = 0.1f  ;
+	private float heaterPower = 1.0f  ;
+
 	public bool isSupra = true;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
@@ -13,10 +20,11 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+		UpdateTemp ();
+
 		if (Input.GetKeyDown (KeyCode.S))
 			isSupra = !isSupra;
-		if (temperature < 0.0f)
-			temperature += Time.deltaTime;
+		
 		if (Input.GetKey (KeyCode.UpArrow))
 		{
 //			rb.velocity = transform.up;
@@ -38,6 +46,22 @@ public class Player : MonoBehaviour {
 			rb.AddForce (transform.right * 10);
 		}
 
+	}
+
+	// This function updates the character's temperature
+	void UpdateTemp()
+	{
+		// Current heater power
+		float currentHeaterPower ;
+
+		// Listen if the Heater key is pressed
+		if (Input.GetKey (KeyCode.P)) {
+			currentHeaterPower = heaterPower ;
+		} else {
+			currentHeaterPower = 0.0f ;
+		}
+		// Compute the current temperature
+		temperature = temperature + Time.deltaTime * ( alpha * (ambientTemperature - temperature) + currentHeaterPower ) ;
 	}
 
 }
