7356a75a1f22a86481f07cb89dbf25b3a6fb2f44
Clement Barbisan <c.barbisan@free.fr>
2017-03-18 13:08:53 +0100

    update ferromagnetism and magnetism field

--- Assets/Scripts/Ferromagnetism.cs
+++ Assets/Scripts/Ferromagnetism.cs
@@ -18,8 +18,7 @@ public class Ferromagnetism : MonoBehaviour {
 		if (other.GetComponent<Player> () && other.GetComponent<Player>().isSupra)
 		{
 			Player user = other.GetComponent<Player> ();
-			RaycastHit2D hit = new RaycastHit2D();
-			Physics2D.Raycast(user.gameObject.transform.position, user.gameObject.transform.position - transform.position, out hit);
+			RaycastHit2D hit = Physics2D.Raycast(user.gameObject.transform.position, user.gameObject.transform.position - transform.position);
 			user.rb.velocity += new Vector2(user.gameObject.transform.position.x - hit.point.x, user.gameObject.transform.position.y - hit.point.y);
 		}
 	}
