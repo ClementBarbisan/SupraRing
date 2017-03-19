143b5f29f38a8e621103339db003ad598d4e25cc
Clement Barbisan <c.barbisan@free.fr>
2017-03-19 16:46:04 +0100

    add switch and door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+	public Switch currentSwitch;
+	private bool open;
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentSwitch.turnOn && !open)
+		{
+			open = true;
+			anim.Play ("door");
+			anim.speed = 1.0f;
+			GetComponent<BoxCollider2D> ().enabled = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+	private bool isInsideAndCharged = false;
+	private Player currentPlayer;
+	public bool turnOn = false;
+	public Sprite onSprite;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Input.GetKeyDown (KeyCode.E) || Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.Joystick1Button16)) && isInsideAndCharged)
+		{
+			turnOn = true;
+			GetComponent<SpriteRenderer> ().sprite = onSprite;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.GetComponent<Player> () && other.GetComponent<Player> ().isCharged)
+		{
+			isInsideAndCharged = true;
+			currentPlayer = other.GetComponent<Player> ();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.GetComponent<Player> ())
+		{
+			isInsideAndCharged = false;
+		}
+	}
+}
